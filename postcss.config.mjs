df0d3764229a7735d5061e7bca075689ec5c5b52
shubhamthakur-2504 <shubhamthakur60278@gmail.com>
2025-04-04 23:44:56 +0530

    project setup

---
+++ postcss.config.mjs
@@ -0,0 +1,7 @@
+// postcss.config.js
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
+export default config;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./app/**/*.{js,jsx}",
+  "./pages/**/*.{js,jsx}",
+  "./components/**/*.{js,jsx}",
+  "./src/**/*.{js,jsx}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+  
