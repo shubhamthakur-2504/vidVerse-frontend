16f708bb0e676f6035c66fb81e7e505729dd11e5
shubhamthakur-2504 <shubhamthakur60278@gmail.com>
2025-10-17 22:57:29 +0530

    feat: added the gernal fetch, delete and post

--- src/lib/utils/functions.js
+++ src/lib/utils/functions.js
@@ -12,7 +12,7 @@ async function fetchVideos(pathName, client = false, cookieStore = null) {
     } else {
         const fetchHeaders = new Headers();
 
-        // 4. Construct the Cookie header from the passed cookieStore
+        // 4. Construct the Cookie header from the passed cookieStore        
         if (cookieStore) {
             const cookieString = cookieStore.getAll().map(cookie => 
                 `${cookie.name}=${cookie.value}`
@@ -25,24 +25,24 @@ async function fetchVideos(pathName, client = false, cookieStore = null) {
             credentials: "include",
         });
 
-        if (!res.ok) {
+        if (res.status !== 200) {
             const errorBody= await res.text();
             console.log("Native fetch failed", errorBody);
-            throw new Error("Failed to fetch videos (server)");
-        }
-
-        const json = await res.json();
+            return {error: errorBody, status: res.status};
+        }        
+        const json = await res.json();        
         return json.data;
     }
 }
 async function gernalFetch(pathName, client = false) {
+    
     if (client) {
         try {
             const res = await apiClient.get(pathName);
             return res.data.data;
         } catch (error) {
             console.error("Axios fetch failed", error);
-            throw new Error("Failed to fetch videos (client)");
+            throw new Error("Failed to fetch route (client) ", pathName);
         }
     } else {
         const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}${pathName}`, {
@@ -51,8 +51,9 @@ async function gernalFetch(pathName, client = false) {
         });
 
         if (!res.ok) {
+            const errorBody= await res.text();
             console.error("Native fetch failed", res.status);
-            throw new Error("Failed to fetch videos (server)");
+            return {error: errorBody, status: res.status};
         }
 
         const json = await res.json();
@@ -60,4 +61,48 @@ async function gernalFetch(pathName, client = false) {
     }
 }
 
-export { fetchVideos, gernalFetch };
+async function gernalPost(pathName, data=null) {
+    if (data) {
+        try {
+            const res = await apiClient.post(pathName, data);
+            return res.data.data;
+        } catch (error) {
+            console.error("Axios post failed", error);
+            const errorBody= await res.text()
+            return {error: errorBody, status: res.status};
+        }
+    }else{
+        try {
+            const res = await apiClient.post(pathName);
+            return res.data.data;
+        } catch (error) {
+            console.error("Axios post failed", error);
+            const errorBody= await res.text()
+            return {error: errorBody, status: res.status};
+        }
+    }
+}
+
+async function gernalDelete(pathName, data=null) {
+    if (data) {
+        try {
+            const res = await apiClient.delete(pathName, data);
+            return res.data.data;
+        } catch (error) {
+            console.error("Axios delete failed", error);
+            const errorBody= await res.text()
+            return {error: errorBody, status: res.status};
+        }
+    }else{
+        try {
+            const res = await apiClient.delete(pathName);
+            return res.data.data;
+        } catch (error) {
+            console.error("Axios delete failed", error);
+            const errorBody= await res.text()
+            return {error: errorBody, status: res.status};
+        }
+    }
+}
+
+export { fetchVideos, gernalFetch, gernalPost, gernalDelete };
