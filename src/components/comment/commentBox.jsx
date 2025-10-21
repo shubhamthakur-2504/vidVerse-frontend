f39aa0a67d77e86d8b7769bc3c6989b0a30e8fbd
shubhamthakur-2504 <shubhamthakur60278@gmail.com>
2025-10-22 00:41:15 +0530

    fix: fix the UI of commentsection

--- src/components/comment/commentBox.jsx
+++ src/components/comment/commentBox.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import ReactionButton from '@components/buttons/reactionButton'
 
-export default function CommentBox({comment}) {    
+export default function CommentBox({ comment }) {
     return (
-        <div className='flex gap-1 p-3'>
+        <div className='flex gap-1 p-3  '>
             <div>
                 <div className='rounded-full w-10 h-10 bg-black overflow-hidden'>
                     <img src={`${comment.userDetails.avatarUrl}`} alt="owner avatar" className='w-10 h-10' />
                 </div>
             </div>
-            <div className='ml-2'>
+            <div className='ml-2 w-[100%]'>
                 <div className='flex gap-2'>
                     <span className='text-xs'>@{`${comment.userDetails.userName}`}</span>
                     <span className='text-xs ml-3'>{`${comment.relativeTime}`}</span>
                     {comment.editStatus && <span className='text-[8px] ml-4'>Edited</span>}
                 </div>
-                <div>
-                    {`${comment.content}`}
-                </div>
-                <div>
-                    <ReactionButton targetId={comment._id} targetType={"Comment"} />
+                <div className='flex '> 
+                    <div className='w-[85%]'>
+                        {`${comment.content}`}
+                    </div>
+                    <div className='w-[10%] h-5  justify-end ml-3'>
+                        <ReactionButton targetId={comment._id} targetType={"Comment"} />
+                    </div>
                 </div>
             </div>
         </div>
