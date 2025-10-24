14fd4a6a7f9f2661579108a08f9c79c8eef1094a
shubhamthakur-2504 <shubhamthakur60278@gmail.com>
2025-10-25 00:03:22 +0530

    chore: laid future groundworks

---
+++ src/app/subscribed/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SubscribedPage() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <p> Will be added on next updates</p>
+    </div>
+  )
+}
---
+++ src/app/tweet/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TweetPage() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <p> Will be added on next updates</p>
+    </div>
+  )
+}
